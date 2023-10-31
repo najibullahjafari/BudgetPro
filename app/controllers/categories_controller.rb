33a0c12558373727b0fa1f3458b7cf4c966c4bb9
najibullah <najib2020202020@gmail.com>
2023-10-31 18:17:01 +0430

    Add vew for categories and transactions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,4 +4,23 @@ class CategoriesController < ApplicationController
   def index
     @categories = current_user.categories
   end
+
+  def new
+    @categories = Category.new
+  end
+
+  def create
+    @categories = current_user.categories.build(category_parms)
+    if @categories.save
+      redirect_to root_path, notice: "Category added"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_parms
+    parms.require(:category).permit(:name, :icon)
+  end
 end
--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,26 @@
 class TransactionsController < ApplicationController
+
+  def index
+    @category = Category.find(params[:category_id])
+    @transactions = @category.transactions.order(created_at: :desc)
+  end
+
+  def new 
+    @transactions = Transactions.new
+  end
+
+  def create
+    @transactions = current_user.transactions.build(transactions_parms)
+    if @transactions.save
+      redirect_to category_transactions_path(@transactions.category), notice: "transaction added"
+    else
+      render: new
+    end
+  end
+
+  private
+
+  def transactions_parms
+    parms.require(:transactions).permit(:name, :amount, :category_id)
+  end
 end
