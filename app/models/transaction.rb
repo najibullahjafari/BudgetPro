fd205a29643619d242935c27ccec4db8c95c89b2
rails <najib2020202020@gmail.com>
2023-11-03 09:16:15 +0430

    UPdate transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,4 @@
 class Transaction < ApplicationRecord
   belongs_to :user
-  has_and_belongs_to_many :categories
+  has_and_belongs_to_many :categories, join_table: 'categories_transactions', class_name: 'Category'
 end
