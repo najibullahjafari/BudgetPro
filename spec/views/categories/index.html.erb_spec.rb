0adce2df8c9f265e0aef68459e789e12db6f59ac
rails <najib2020202020@gmail.com>
2023-11-03 23:21:44 +0430

    Update spec of catergory veiw_index page

--- spec/views/categories/index.html.erb_spec.rb
+++ spec/views/categories/index.html.erb_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'categories/index', type: :view do
+  let(:user) { create(:user) }
   before(:each) do
     assign(:categories, [])
   end
 
   context 'when there are no categories' do
-    it 'displays a message if there are no categories' do
+    it 'displays a message to show that there is just the h1 element' do
       render
-      expect(rendered).to have_content('')
+      expect(rendered).to have_content('Categories') # becasue there is h1 element with this content
     end
   end
 end
