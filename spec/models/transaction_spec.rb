3dea2195e915ea058100654f4310adccb1b632a5
najibullah <najib2020202020@gmail.com>
2023-11-03 10:15:36 +0430

    Update specs

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -2,19 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
   before do
-    @user = User.create(full_name: 'Alice Smith', email: 'alice@example.com', password: 'password456')
+    @user = User.create(full_name: 'Najib', email: 'najib@example.com', password: 'password456')
     @category = Category.create(name: 'Sample Category', icon: 'fa-icon-name', user: @user)
     @transaction = Transaction.create(name: 'Sample Transaction', amount: 100.00, user: @user, categories: [@category])
-  end
-
-  it 'name should be present' do
-    @transaction.name = nil
-    expect(@transaction).to_not be_valid
-  end
-
-  it 'amount should be present' do
-    @transaction.amount = nil
-    expect(@transaction).to_not be_valid
   end
 
   it 'should be valid' do
