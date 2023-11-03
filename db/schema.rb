dd7dd4980443cb4d56b62e4483a0eaa4ed5035bc
rails <najib2020202020@gmail.com>
2023-11-03 09:17:07 +0430

    UPdate rails_helper

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_31_140115) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_02_163328) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_31_140115) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "categories_transactions", force: :cascade do |t|
+    t.bigint "category_id", null: false
+    t.bigint "transaction_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_categories_transactions_on_category_id"
+    t.index ["transaction_id"], name: "index_categories_transactions_on_transaction_id"
   end
 
   create_table "transactions", force: :cascade do |t|
@@ -53,6 +62,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_31_140115) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "categories_transactions", "categories"
+  add_foreign_key "categories_transactions", "transactions"
   add_foreign_key "transactions", "categories"
   add_foreign_key "transactions", "users"
 end
