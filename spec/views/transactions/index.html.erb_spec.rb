e7c08b888ad7491d94f8004ad41af7c0c249f725
rails <najib2020202020@gmail.com>
2023-11-03 23:22:12 +0430

    Update spec of transaction view page

--- spec/views/transactions/index.html.erb_spec.rb
+++ spec/views/transactions/index.html.erb_spec.rb
@@ -2,15 +2,26 @@ require 'rails_helper'
 
 RSpec.describe 'transactions/index', type: :view do
   before(:each) do
+    category = create(:category, name: 'Category Name')
     assign(:budget_transactions, [])
-    assign(:category, create(:category, name: 'Category Name'))
+    assign(:category, category)
   end
 
-  context 'when there are no transactions' do
-    it 'displays a message if there are no transactions' do
+
+  context 'when there are no transactions but have the contents' do
+    it 'displays a message if it has the h1 element that is "Transactions" ' do
       render
-      expect(rendered).to have_content('Transactions for Category Name')
+      expect(rendered).to have_content('Transactions')
+    end
+
+    it 'displays a message if there is Total Amount content' do
+      render
       expect(rendered).to have_content('Total Amount:')
     end
+  end
+
+  it 'displays a message if there is a "Back" button' do
+    render
+    expect(rendered).to have_link('Back', href: categories_path)
   end
 end
