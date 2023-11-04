3a92198ff48e1e181a5aafa70c5c6d313b9b362a
Najibullah Jafari <najib2020202020@gmail.com>
2023-11-04 19:14:54 +0430

    Delete test directory

--- test/controllers/static_pages_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test 'should get splash' do
-    get static_pages_splash_url
-    assert_response :success
-  end
-end
